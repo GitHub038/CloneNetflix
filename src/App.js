d93429737baec467bc16f5af0c2660eea7a15f30
sao <c_pasmoi@outlook.com>
2023-02-11 22:05:40 +0100

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.scss';
+import "./App.scss";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div className="App">toto
+    <div className="App">
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./Nav.scss";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
+function Nav() {
+  const [navBlack, setNavBlack] = useState(false);
+  const [toogleMenu, setToogleMenu] = useState(false);
+
+  const transitionNav = () => {
+    // Lorsqu'on scroll verticalement
+    window.scrollY > 100 ? setNavBlack(true) : setNavBlack(false);
+  };
+
+  //lorsqu'on scroll on appel la fonction
+  useState(() => {
+    // eslint-disable-next-line no-unused-expressions
+    document.addEventListener[("scroll", transitionNav)];
+  });
+
+  const handleClick = () => {
+    toogleMenu ? setToogleMenu(false) : setToogleMenu(true);
+  };
+
+  return (
+    <div
+      className={`nav ${
+        navBlack || toogleMenu ? "nav--black" : "nav--transparent"
+      } ${toogleMenu && "show"}`}
+    >
+      <button className="nav__burger" onClick={handleClick}>
+        <MenuIcon />
+      </button>
+      <img src="./images/logo.png" className="nav__logo" alt="Netflix" />
+      <nav className="nav__links">
+        <a href="/" className="nav__link">
+          Accueil
+        </a>
+        <a href="/" className="nav__link">
+          Séries
+        </a>
+        <a href="/" className="nav__link">
+          Films
+        </a>
+      </nav>
+      <div className="nav__actions">
+        <a href="/" className="nav_action">
+          <SearchIcon />
+        </a>
+        <a href="/" className="nav_action">
+          DIRECT
+        </a>
+        <a href="/" className="nav_action">
+          <CardGiftcardIcon />
+        </a>
+        <a href="/" className="nav_action">
+          <NotificationsIcon />
+        </a>
+        <a href="/" className="nav_action">
+          <img src="./images/avatar.jpg" alt=""></img>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
