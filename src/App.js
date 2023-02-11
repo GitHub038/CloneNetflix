22825884df15c700bf4e8b139c52bf55853da3b4
sao <c_pasmoi@outlook.com>
2023-02-11 22:30:27 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Footer />
     </div>
   );
 }
