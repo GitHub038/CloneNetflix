d265ab55e153b4bcab6eb84e713c2b3e3ba233f9
sao <c_pasmoi@outlook.com>
2023-02-15 21:08:23 +0100

    display film by category

---
+++ src/config/Requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "dab1d5ba4414fad9463e93741d727627";
+const baseURL = "https://api.themoviedb.org/3";
+
+// Ici toutes nos requetes vers l'API
+const requests = {
+  fetchTrending: `${baseURL}/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `${baseURL}/trending/all/week?api_key=${API_KEY}`,
+  fetchTopRated: `${baseURL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=28`,
+  fetchComedyMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=35`,
+  fetchHorrorMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=27`,
+  fetchRomanceMovies: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=10749`,
+  fetchDocumentaries: `${baseURL}/discover/movie?api_key=${API_KEY}&width_genres=99`,
+};
+
+export default requests;
